1c8926f919b1776946f8abffb40da51ffbdf4f12
radzun9080002222-ship-it <radzun9080002222@gmail.com>
2026-09-16 15:14:59 +0300

    Add Tula Metrika counter 112711902

---
+++ public/metrika.js
@@ -0,0 +1,8 @@
+window.ym = window.ym || function () { (window.ym.a = window.ym.a || []).push(arguments); };
+window.ym.l = 1 * new Date();
+window.ym(112711902, 'init', { webvisor: true, clickmap: true, accurateTrackBounce: true, trackLinks: true });
+var s = document.createElement('script');
+s.async = 1;
+s.src = 'https://mc.yandex.ru/metrika/tag.js';
+document.head.appendChild(s);
+
